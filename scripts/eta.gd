ae1fe92b1d67a7918513c0ae232afbd9196cda64
SmXThing <aogletree2005@gmail.com>
2026-02-12 21:46:45 -0600

    Added Player Sprites and Test Room Scenes

---
+++ scripts/eta.gd
@@ -0,0 +1,109 @@
+extends CharacterBody2D
+
+const speed = 150.0
+const dash_speed = 400.0
+const wall_jump_distance = 50
+const jump_height = -350.0
+const gravity = 20.0
+
+var var_gravity = gravity
+
+var moving: bool
+var can_dash: bool = true
+
+var can_jump: bool = true
+var jump_applied: bool = false
+var jump_buffer: bool = false
+var jump_locked: bool = false
+
+var wall_jump_applied: bool = false
+
+var last_direction: float = 1.0
+
+var jump_threshold: float = -100
+
+func _ready() -> void:
+	$Sprites.play("idle")
+
+func _physics_process(delta: float) -> void:
+	if !is_on_floor():
+		if is_on_wall():
+			if velocity.y <= 150:
+				velocity.y += var_gravity
+			else:
+				velocity.y = 150
+		else:
+			if can_jump:
+				if $CoyoteTime.is_stopped():
+					$CoyoteTime.start()
+			if velocity.y <= 300:
+				velocity.y += var_gravity
+	elif is_on_floor() || is_on_wall():
+		can_jump = true
+		$CoyoteTime.stop()
+		if jump_buffer:
+			jump_action()
+
+	if Input.is_action_just_pressed("SPACE"):
+		if !jump_locked:
+			if can_jump:
+				jump_action()
+			elif is_on_wall_only():
+				wall_jump()
+			else:
+				$JumpBuffer.start()
+				jump_buffer = true
+	
+	if Input.is_action_just_pressed("SHIFT") && can_dash:
+		can_dash = false
+		jump_locked = true
+		$DashCooldown.start()
+		$DashTimer.start()
+		var_gravity = 0
+		velocity.y = 0 
+		velocity.x = last_direction * dash_speed
+	
+	var direction := Input.get_axis("KEY_A", "KEY_D")
+	
+	if direction:
+		moving = true
+		last_direction = direction
+		if abs(velocity.x) > speed:
+			velocity.x = move_toward(velocity.x, speed, 10)
+		elif !is_on_floor():
+			if abs(velocity.x) <= speed:
+				velocity.x += direction * 15
+		else:
+			velocity.x = direction * speed
+	else:
+		moving = false
+		if !is_on_floor() || !$DashTimer .is_stopped():
+			if abs(velocity.x) > 0:
+				velocity.x = move_toward(velocity.x, 0, 15)
+		else:
+			velocity.x = move_toward(velocity.x, 0, speed)
+	
+	$Sprites.flip_h = !bool((last_direction + 1) / 2)
+
+	move_and_slide()
+
+func wall_jump() -> void:
+	wall_jump_applied = true
+	velocity.x = speed * -last_direction
+	velocity.y = jump_height
+
+func jump_action() -> void:
+	velocity.y = jump_height
+
+func _on_coyote_time_timeout() -> void:
+	can_jump = false
+
+func _on_jump_buffer_timeout() -> void:
+	jump_buffer = false
+
+func _on_dash_timer_timeout() -> void:
+	var_gravity = gravity
+	jump_locked = false
+
+func _on_dash_cooldown_timeout() -> void:
+	can_dash = true
